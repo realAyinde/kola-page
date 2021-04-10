ad3e92126b175648228da7e8867ebdb7bc7355b2
Ayinde Sodiq <58987770+realAyinde@users.noreply.github.com>
2021-04-10 13:16:32 +0100

    make changes

--- src/graph.js
+++ src/graph.js
@@ -1,43 +1,4 @@
 // window.onload = function () {
 //   var optionsV = document.getElementsByClassName("options-v")[0];
-  var optionsV = document.getElementById("month-options");
 
-  function next() {
-    var ul = optionsV.getElementsByTagName("ul")[0];
-    var active = ul.getElementsByClassName("active")[0];
-    if (active.nextElementSibling) {
-      active.className = "option-item";
-      active.nextElementSibling.className += " active";
-    }
-  }
-
-  function previous() {
-    var ul = optionsV.getElementsByTagName("ul")[0];
-    var active = ul.getElementsByClassName("active")[0];
-    if (active.previousElementSibling) {
-      active.className = "option-item";
-      active.previousElementSibling.className += " active";
-    }
-  }
-
-  optionsV.querySelector('[to="left"]').addEventListener("click", previous, true);
-    optionsV.querySelector('[to="right"]').addEventListener("click", next, false);
-  
-
-//   if (optionsV !== null) {
-    
-//   }
-
-  var filterList = document.getElementsByClassName("filter-list")[0];
-  var filterItems = filterList.getElementsByClassName("filter-item");
-
-  for (var i = 0; i < filterItems.length; i++) {
-    filterItems[i].addEventListener("click", function () {
-      var current = filterList.getElementsByClassName("selected");
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" selected", "");
-      }
-      this.className += " selected";
-    });
-  }
 // }
