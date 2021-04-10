57df4711a5cc3baf8365d8065b717b6882df741a
Ayinde Sodiq <58987770+realAyinde@users.noreply.github.com>
2021-04-10 13:42:09 +0100

    replaced eventlistener with onclick

--- src/graph.js
+++ src/graph.js
@@ -1,4 +1,42 @@
 // window.onload = function () {
 //   var optionsV = document.getElementsByClassName("options-v")[0];
+  var optionsV = document.getElementById("month-options");
 
+  function next() {
+    var ul = optionsV.getElementsByTagName("ul")[0];
+    var active = ul.getElementsByClassName("active")[0];
+    if (active.nextElementSibling) {
+      active.className = "option-item";
+      active.nextElementSibling.className += " active";
+    }
+  }
+
+  function previous() {
+    var ul = optionsV.getElementsByTagName("ul")[0];
+    var active = ul.getElementsByClassName("active")[0];
+    if (active.previousElementSibling) {
+      active.className = "option-item";
+      active.previousElementSibling.className += " active";
+    }
+  }
+
+  optionsV.querySelector('[to="left"]').onclick = () => {
+      previous()
+    };
+    optionsV.querySelector('[to="right"]').onclick = () => {
+        next()
+      };;
+
+  var filterList = document.getElementsByClassName("filter-list")[0];
+  var filterItems = filterList.getElementsByClassName("filter-item");
+
+  for (var i = 0; i < filterItems.length; i++) {
+    filterItems[i].onclick = () => {
+      var current = filterList.getElementsByClassName("selected");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" selected", "");
+      }
+      this.className += " selected";
+    };
+  }
 // }
